ecb3839f3ee9e96523b897f763b8993aab9d3b0b
Ondrej Lexa <lexa.ondrej@gmail.com>
2021-05-25 18:57:15 +0200

    fix

--- tables/units/units.php
+++ tables/units/units.php
@@ -2,7 +2,7 @@
 class tables_units {
 
 	function beforeSave($record){
-	    $user =& Dataface_AuthenticationTool::getInstance()->getLoggedInUsername();
+	    $user = Dataface_AuthenticationTool::getInstance()->getLoggedInUsername();
         $record->setValue('ModifiedBy', $user);
 	}
 	
